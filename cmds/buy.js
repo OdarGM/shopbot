bd0a37764ff68c72d8c414a492865a01815a26c9
Odar <28188739+OdarGM@users.noreply.github.com>
2018-05-10 20:48:44 +0300

    Update buy.js

--- cmds/buy.js
+++ cmds/buy.js
@@ -13,8 +13,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Bot Commands", "Custom commands", inline)
     .addField("XP System", "Optional", inline)
     .addField("Private bot", "Yes only the bot owner can invite it!")
-    .addField("Pricing", "Use `#prices` to see them", inline)
-    .addField("Order", "To order the bot use `#order`", inline)
+    .addField("Pricing", "Use `]prices` to see them", inline)
+    .addField("Order", "To order the bot use `]order`", inline)
     .addField("Questions", "If you have any questions feel free to ask in the [Support Server](https://discord.gg/zvvasbc)")
     .setFooter("ShopBot by Odar", "https://cdn.discordapp.com/avatars/442410451121209365/4272634f3ab276ff263ff16ca3aa560a.png?size=2048")
 
